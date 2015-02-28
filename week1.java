d177fa1bc17810b13c93df9c5f19116525327fd0
abosd26 <d199469@yahoo.com.tw>
2015-03-01 00:22:51 +0800

    week1 homework

---
+++ week1.java
@@ -0,0 +1,12 @@
+class week1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("《虢州後亭送李判官》");
+		System.out.println("作者：岑參");
+		System.out.println("西原驛路挂城頭");
+		System.out.println("客散紅亭雨未收");
+		System.out.println("君去試看汾水上");
+		System.out.println("白雲猶似漢時秋");
+	}
+}
